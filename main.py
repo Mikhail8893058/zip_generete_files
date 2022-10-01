5fbb71b2237891891f6d336480a15800e739ced2
Mihail <mishadyshlevich@mail.ru>
2022-10-01 11:33:34 +0300

    main

---
+++ main.py
@@ -0,0 +1,7 @@
+import getting_text as gt
+from seminar_05.DZ.RLE.getting_text import get_txt
+from seminar_05.DZ.RLE.zip_generete import compress_zip
+import zip_generete as zp
+
+get_txt()
+compress_zip()
